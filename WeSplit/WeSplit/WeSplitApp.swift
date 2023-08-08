218a3f878311c870e5e733a314778a9a5b9f9119
victorHSFerraz <victorf178@gmail.com>
2023-08-07 23:45:01 -0300

    first commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Victor Ferraz on 04/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
