3e285d125c6102b14ccbb7ca0a9569e35f86311e
victorHSFerraz <victorf178@gmail.com>
2023-08-19 17:02:32 -0300

    WordScramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Victor Ferraz on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
